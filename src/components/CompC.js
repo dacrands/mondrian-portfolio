58b3685c43b545c70c80ce841fad0043a350e16e
da crands <davecrands@gmail.com>
2019-04-23 10:57:44 -0400

    add CompC content

--- src/components/CompC.js
+++ src/components/CompC.js
@@ -5,25 +5,61 @@ export default () => (
     <div className="canvas">
       <div className="canvas__top">
         <div className="canvas__top-block">
-        <h2>Projects</h2>
-        <p>Most of my projects
+          <h2>            
+            My apps use React (GatsybJS), Flask, MySQL, Docker,
+            Netlify, and DigitalOcean.
+          </h2>
+        {/* <p>Most of my projects
           are built with react.
-        </p>
+        </p> */}
         </div>
-        <div className="canvas__top-block"></div>
+        <div className="canvas__top-block">
+        </div>
       </div>
       <div className="canvas__mid">
-        <div className="canvas__mid-block"></div>
-        <div className="canvas__mid-block"></div>
+        <div className="canvas__mid-block">
+        </div>
+        <div className="canvas__mid-block">
+        </div>
       </div>
       <div className="canvas__bottom">
         <div className="canvas__bottom-block yellow"></div>
-        <div className="canvas__bottom-block"></div>
+        <div className="canvas__bottom-block">
+          <h3>Skills</h3>
+          <ul className="list">
+            {
+              [
+                "Responsive web design",
+                "RESTful APIs",
+                "Ajax",
+                "VPS hosting",
+                "User authentication",                
+              ].map(skill => (
+                <li>{skill}</li>
+              ))
+            }
+          </ul>
+        </div>
         <div className="canvas__bottom-grid">
           <div className="bottom-grid__block"></div>
           <div className="bottom-grid__block"></div>
         </div>
-        <div className="canvas__bottom-block"></div>
+        <div className="canvas__bottom-block">
+          <h3>Languages</h3>
+            <ul className="list">
+              {
+                [
+                  "JavaScript",
+                  "Python",
+                  "SQL",
+                  "CSS",
+                  "HTML",
+                ].map(skill => (
+                  <li>{skill}</li>
+                ))
+              }
+            </ul>
+        </div>
       </div>
     </div>
   </section>
