73856965e84c6c57415d82be7edcdf5742aba6f5
da crands <davecrands@gmail.com>
2019-04-22 23:02:37 -0400

    Add children to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "../styles/main.scss";
 
-export default () => (
+export default ({ children }) => (
   <div className="container">
-    <h1>Hello</h1>
+    {children}
   </div>
 )
