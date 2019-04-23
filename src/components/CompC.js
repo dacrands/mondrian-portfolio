34fdc44dd108f4fb6244792ac5d5632862263c33
da crands <davecrands@gmail.com>
2019-04-23 09:52:23 -0400

    add Composition C component and styles

---
+++ src/components/CompC.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default () => (
+  <section className="comp__c">
+    <div className="canvas">
+      <div className="canvas__top">
+        <div className="canvas__top-block">
+        <h2>Projects</h2>
+        <p>Most of my projects
+          are built with react.
+        </p>
+        </div>
+        <div className="canvas__top-block"></div>
+      </div>
+      <div className="canvas__mid">
+        <div className="canvas__mid-block"></div>
+        <div className="canvas__mid-block"></div>
+      </div>
+      <div className="canvas__bottom">
+        <div className="canvas__bottom-block yellow"></div>
+        <div className="canvas__bottom-block"></div>
+        <div className="canvas__bottom-grid">
+          <div className="bottom-grid__block"></div>
+          <div className="bottom-grid__block"></div>
+        </div>
+        <div className="canvas__bottom-block"></div>
+      </div>
+    </div>
+  </section>
+)
