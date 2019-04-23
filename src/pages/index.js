a38b98ca19b48c1cd63e3e70ee22f2c12bcfa9da
da crands <davecrands@gmail.com>
2019-04-23 10:57:30 -0400

    Include Composition C in /index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import CompThree from "../components/CompThree"
+import CompC from "../components/CompC"
 
 export default () => (
   <Layout>
     <CompThree />
+    <CompC />
   </Layout>
 )
